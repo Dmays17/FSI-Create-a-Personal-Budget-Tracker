f600043e381aed837c89783798bbc27ff8228501
David Mays <dmaysengineering@gmail.com>
2022-04-04 17:14:52 -0500

    create the code to run the numbers for the platform

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+for(let i=0 ;i < weeklyExpenseQuestions.length; i ++){
+    let weeklyAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + weeklyAnswer    
+}
+
+for(let i=0;i < monthlyExpenseQuestions.length; i++){
+    let monthlyAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + monthlyAnswer
+}
+
+for(let i=0;i < annualExpenseQuestions.length; i++){
+    let annualAnswer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + annualAnswer
+}
